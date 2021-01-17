c5f8c9fb18ec6e785bb34a7b8b9f39c4bfcdc477
Myagkov Andrey <amyagkov@itoolabs.com>
2021-01-18 02:08:15 +0300

    add api

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './Menu.scss';
 
-const Menu = () => (
-  <nav className="menu">
-    <div className="menu__list">
-      <ul />
-    </div>
-  </nav>
-);
+const url = new URL('https://jsonplaceholder.typicode.com');
+
+const Menu = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(async () => {
+    try {
+      const response = await axios.get(`${url}users`);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <nav className="menu">
+      <div className="menu__list">
+        <ul>
+          {
+            data ? (
+              data.map((el) => (
+                <li key={el.id}>
+                  <a href={el.id}>{el.name}</a>
+                  <hr />
+                </li>
+              ))
+            ) : null
+          }
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Menu;
